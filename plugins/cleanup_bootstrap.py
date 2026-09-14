73ad14fce60a9c2ecf336d5e52a7ed5606514d90
iqleveling <iqleveling@gmail.com>
2026-09-14 22:21:53 +0530

    fix: remember each incoming user message for cleanup

--- plugins/cleanup_bootstrap.py
+++ plugins/cleanup_bootstrap.py
@@ -1,9 +1,13 @@
 from pyrogram import Client, filters
 
-from helper.message_cleanup import install_auto_cleanup
+from helper.message_cleanup import install_auto_cleanup, remember_user_message
 
 
 @Client.on_message(filters.private, group=-100000)
 async def initialize_private_cleanup(client, message):
     """Install cleanup before normal private-chat handlers send their replies."""
     install_auto_cleanup(client)
+    try:
+        await remember_user_message(message.chat.id, message.id)
+    except Exception:
+        pass
